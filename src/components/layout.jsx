cbd7f043378e68c44f7085da632ad9caada1b614
sonseong10 <sonseong10@gmail.com>
2021-09-13 16:55:36 +0900

    💄 Implement grid system layout

---
+++ src/components/layout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { css } from "@emotion/react";
+import media from "../assets/styles/constants/media";
+
+const Layout = ({ children }) => {
+  return <div css={container}>{children}</div>;
+};
+
+export default Layout;
+
+const Main = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+Layout.Main = Main;
+
+const container = css`
+  width: 100%;
+  margin: 0 auto;
+  ${media.tablet} {
+    max-width: 960px;
+  }
+  ${media.desktop} {
+    max-width: 1140px;
+  }
+`;
