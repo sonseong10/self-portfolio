615ea4f93b3e888db6d67c6de3f94aeb67a5fdc9
sonseong10 <sonseong10@gmail.com>
2021-09-13 16:57:19 +0900

    🚧 Apply global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import "./App.css";
+import React from "react";
+import { Global, css } from "@emotion/react";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Self Portfolio</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1> Self Portfolio</h1>
+      </div>
+      <Global styles={globalStyle} />
+    </>
   );
 }
+
+const globalStyle = css`
+  html,
+  body,
+  #root {
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    box-sizing: border-box;
+    * {
+      box-sizing: inherit;
+    }
+  }
+
+  .sr-only {
+    position: absolute !important;
+    width: 1px !important;
+    height: 1px !important;
+    padding: 0 !important;
+    margin: -1px !important;
+    overflow: hidden !important;
+    clip: rect(0, 0, 0, 0) !important;
+    white-space: nowrap !important;
+    border: 0 !important;
+  }
+`;
 
 export default App;
