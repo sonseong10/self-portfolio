4d820b1afc6b88949e4ddf583fe9f291ddba9f83
sonseong10 <sonseong10@gmail.com>
2021-09-28 22:02:31 +0900

    🔧 Implement firebase config

---
+++ src/service/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
