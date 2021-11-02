b076be469e9565a16871173136a595bcdce93f34
sonseong10 <sonseong10@gmail.com>
2021-11-02 17:09:20 +0900

    🔥 Remove carousel setting

--- src/utils/slick-carousel.js
+++ src/utils/slick-carousel.js
@@ -9,38 +9,6 @@ const settings = {
   slidesToScroll: 1,
 };
 
-const mainSettings = {
-  ...settings,
-  arrows: true,
-  nextArrow: <CarouselArrow.NextArrow />,
-  prevArrow: <CarouselArrow.PrevArrow />,
-
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        dots: true,
-        arrows: false,
-      },
-    },
-  ],
-};
-
-const infoSettings = {
-  ...settings,
-  speed: 0,
-  draggable: false,
-  touchMove: false,
-};
-
-const dotSettings = {
-  ...settings,
-  centerPadding: "0",
-  centerMode: true,
-  slidesToShow: 3,
-  focusOnSelect: true,
-};
-
 const artworkSettings = {
   ...settings,
   centerPadding: "0",
@@ -70,10 +38,4 @@ const artDotsSettings = {
   touchMove: false,
 };
 
-export {
-  dotSettings,
-  mainSettings,
-  infoSettings,
-  artworkSettings,
-  artDotsSettings,
-};
+export { artworkSettings, artDotsSettings };
