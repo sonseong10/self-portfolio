1d0bffcc2bd527ca83e9ed000ce27b5cf6af5e0b
sonseongyeol <sonseong10@gmail.com>
2025-06-17 18:16:09 +0900

    Remove warning meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,6 @@ export default async function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Self Portfolio" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="손성열 포트폴리오" />
         <link
